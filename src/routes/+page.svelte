688f59e4ed12643a999bceb54120536db27d2b4d
Micah Russell <108246578+thknkg@users.noreply.github.com>
2023-08-18 17:51:47 -0500

    comment out another button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 	<div class="space-y-5">
 		<h1 class="h1">Project Rio</h1>
 		<ul class="list-nav flex">
-			<li><button class="btn variant-filled-primary m-2"><a href="/community">community</a></button></li>
+<!--			<li><button class="btn variant-filled-primary m-2"><a href="/community">community</a></button></li>-->
 <!--			<li><button class="btn variant-filled-primary m-2"><a href="/stats">stats</a></button></li>-->
 			<li><button class="btn variant-filled-primary m-2"><a href="/modes/all">modes</a></button></li>
 <!--			<li><button class="btn variant-filled-primary m-2"><a href="/tag_set/create">create tagset</a></button></li>-->
