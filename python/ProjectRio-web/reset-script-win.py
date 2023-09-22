5dd4e0a17e803e36b667335d1e33579b63461a1b
Micah Russell <108246578+thknkg@users.noreply.github.com>
2023-09-21 20:18:51 -0500

    cleanup

--- python/ProjectRio-web/reset-script-win.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-#remove old database instance
-subprocess.run(['del', 'app\db.sqlite3'], shell=True)
-
-#open server/create new db instance
-subprocess.run(['pipenv', 'run', 'py', '--3', 'wsgi.py'])
--- python/ProjectRio-web/wsgi.py
+++
@@ -1,6 +0,0 @@
-from app import init_app
-
-app = init_app()
-
-if __name__ == '__main__':
-    app.run()
