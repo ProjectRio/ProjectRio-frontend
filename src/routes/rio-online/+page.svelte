e54e8b8ceafc201fe469c24f5b483b734a7a0cc4
Micah Russell <108246578+thknkg@users.noreply.github.com>
2023-09-21 20:55:07 -0500

    remove module causing build error

--- src/routes/rio-online/+page.svelte
+++ src/routes/rio-online/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import RioLogo from "$lib/images/RioLogo.svg?raw"
-	import { IconCheckbox, IconChevronsRight } from '@tabler/icons-svelte'
+	// import { IconCheckbox, IconChevronsRight } from '@tabler/icons-svelte'
 	import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
 	export let data
 </script>
@@ -57,23 +57,23 @@
 			<ul class="list flex-col m-4">
 				
 					<li>
-						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>
+<!--						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>-->
 						<span class="flex-auto">Optimized online play</span>
 					</li>
 					<li>
-						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>
+<!--						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>-->
 						<span class="flex-auto">Game stat files</span>
 					</li>
 					<li>
-						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>
+<!--						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>-->
 						<span class="flex-auto">Ranked online</span>
 					</li>
 					<li>
-						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>
+<!--						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>-->
 						<span class="flex-auto">Public stat database</span>
 					</li>
 					<li>
-						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>
+<!--						<span class="badge-icon"><IconCheckbox color="limegreen" class={"icon-tada"} /></span>-->
 						<span class="flex-auto">Gameplay mods</span>
 					</li>
 					<!-- ... -->
@@ -86,56 +86,56 @@
 			<nav slot="content" class="list-nav">
 				<li>
 					<a class="flex-auto" href="https://roeming.github.io/MSSB_Batting_Calculator/">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Roeming's Batting Calculator</span>
 
 					</a>
 				</li>
 				<li>
 					<a class="flex-auto" href="https://docs.google.com/spreadsheets/d/16cEcCq-Gkudx5ESfqzS0MJlQI7WTvSIWsHVZS8jv750/edit?usp=sharing">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Datamined Stats & Info</span>
 
 					</a>
 				</li>
 				<li>
 					<a class="flex-auto" href="https://docs.google.com/spreadsheets/d/16cEcCq-Gkudx5ESfqzS0MJlQI7WTvSIWsHVZS8jv750/edit?usp=sharing">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Mario Superstar Baseball Wiki</span>
 
 					</a>
 				</li>
 				<li>
 					<a class="flex-auto" href="https://www.reddit.com/r/MarioBaseball/comments/rohg6j/competitive_mario_superstar_baseball_the_master/?utm_source=share&utm_medium=web2x&context=3">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Competitive Guide/Tutorial</span>
 
 					</a>
 				</li>
 				<li>
 					<a class="flex-auto" href="https://docs.google.com/spreadsheets/d/13zijF8WoYxB3Z4GQRI7wFH6GMEkEqY_CaUQhd-WJQ2M/edit#gid=1510496567">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Neil's Stat Cards</span>
 
 					</a>
 				</li>
 				<li>
 					<a class="flex-auto" href="https://pastebin.com/cPBAFkKf">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Gecko Codes</span>
 
 					</a>
 				</li>
 				<li>
 					<a class="flex-auto" href="https://www.youtube.com/c/DingerCity">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Dinger City Youtube Channel</span>
 
 					</a>
 				</li>
 				<li>
 					<a class="flex-auto" href="https://www.youtube.com/playlist?list=PL8i4ehYol9H0TNb9NrSSP_agA9_OJfe7Y">
-						<span class="badge bg-primary-500"><IconChevronsRight/></span>
+<!--						<span class="badge bg-primary-500"><IconChevronsRight/></span>-->
 						<span class="flex-auto">Joe Baseball's Basics Series</span>
 
 					</a>
