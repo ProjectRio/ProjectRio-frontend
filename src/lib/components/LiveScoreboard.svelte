f2295f0cef3623ad625c750b1d17feef3f0c6286
Nicholas Lannutti <nlannutti97@gmail.com>
2023-12-28 19:14:01 -0500

    Added crash checks

--- src/lib/components/LiveScoreboard.svelte
+++ src/lib/components/LiveScoreboard.svelte
@@ -1,4 +1,9 @@
 <script lang='ts'>
+
+/*
+Improvements
+- hide confirmed crashed games
+*/
 	import { onMount } from "svelte";
     import { getAllTagSets } from '$lib/helpers/tagNames';
     import { tagsets } from '$lib/stores/tagsets';
@@ -20,6 +25,11 @@
     let display_s: number = 5;
     let lastAPICall: number;
     let currentLiveGame: boolean = false;
+    let liveGameTimeframe: number = 24*60*60
+    let currentLiveList: any
+    let newLiveList: any
+    let suspectedCrashedGameIndexes: number[] = []
+    let confirmedCrashedGameIndexes: number[] = []
 
     onMount(async () => {
         console.log("Onmount started")
@@ -28,16 +38,55 @@
 
         const dataRefreshInterval = setInterval(async () => {
             console.log("Data interval ran")
-            if ((currentLiveGame && Date.now() - 20*1000 > lastAPICall) || //if there is a current live game, refresh it periodically, or
+            if ((currentLiveGame && Date.now() - 5*1000 > lastAPICall) || //if there is a current live game, refresh it periodically, or
                 (!currentLiveGame && Date.now() - 10*60*1000 > lastAPICall)) { //if there isn't a live game, then check for new ones less frequently
-                await getLiveGames(105*60)
+                currentLiveList = $liveGameList
+                await getLiveGames(liveGameTimeframe)
                 lastAPICall = Date.now()
+                newLiveList = $liveGameList
                 currentLiveGame = $liveGameList.length > 0; //check if any live games were returned.
+
+                function isCrash (oldImage: any, newImage: any) {
+                    return (oldImage.inning === newImage.inning &&
+                        oldImage.half_inning === newImage.half_inning &&
+                        oldImage.batter === newImage.batter &&
+                        oldImage.pitcher === newImage.pitcher)
+                }
+
+                //Check for possible crashes
+                newLiveList.forEach((game: { start_time: number; inning: number; half_inning: number; batter: number; pitcher: number;}, newGameIndex: number) => {
+                    // fund matching old game
+                    var matchingOldGameIndex = currentLiveList.findIndex((oldGame: { start_time: any; }) => {
+                        return game.start_time === oldGame.start_time
+                    })
+                    console.log("Match", matchingOldGameIndex)
+
+                    if (isCrash(currentLiveList[matchingOldGameIndex], game)) {
+                        //if index is already a suspected crash, then mark as confirmed crash
+                        console.log("markec as crashed", suspectedCrashedGameIndexes,suspectedCrashedGameIndexes.includes(newGameIndex))
+                        if (suspectedCrashedGameIndexes.includes(newGameIndex)) {
+                            console.log("Went to confirmed crash logic")
+                            confirmedCrashedGameIndexes.indexOf(newGameIndex) === -1 ? confirmedCrashedGameIndexes.push(newGameIndex) : void(0);
+                        } else { //if not already suspected, then add to suspected list
+                            console.log("Went to suspected crash logic")
+                            suspectedCrashedGameIndexes.indexOf(newGameIndex) === -1 ? suspectedCrashedGameIndexes.push(newGameIndex) : void(0);
+                        }
+                    } else { // if not a crash, the remove index from crashed lists if found
+                        var indexToRemove = suspectedCrashedGameIndexes.indexOf(newGameIndex);
+                        if (indexToRemove > -1) {
+                            suspectedCrashedGameIndexes.splice(indexToRemove, 1);
+                        }
+                        indexToRemove = confirmedCrashedGameIndexes.indexOf(newGameIndex);
+                        if (indexToRemove > -1) {
+                            suspectedCrashedGameIndexes.splice(indexToRemove, 1);
+                        }
+                    }
+                }); 
+                console.log(suspectedCrashedGameIndexes, confirmedCrashedGameIndexes)
             }
-        
             console.log("Data interval finished")
         }, 10*1000);
-        await getLiveGames(105*60); //initial API call
+        await getLiveGames(liveGameTimeframe); //initial API call
         lastAPICall = Date.now()
         console.log("Initial API request finished", $liveGameList)
         liveGame = $liveGameList[0]; // set initial game to display
@@ -95,7 +144,7 @@
                 <div class="character"><img src={`/src/lib/images/Characters/${(liveGame.half_inning === 0) ? characters[liveGame[`home_roster_${liveGame.pitcher}_char`]] : characters[liveGame[`home_roster_${liveGame.batter}_char`]]}.png`} alt="current batter/pitcher"></div>
                 <div class="bases-count">
                     <div class="bases"><img src={`/src/lib/images/Bases/R${((liveGame.runner_on_first) ? "1" : "") + ((liveGame.runner_on_second) ? "2" : "") + ((liveGame.runner_on_third) ? "3" : "")}.png`} alt="current runners on base"></div>
-                    <div class="count">{(liveGame.outs===0) ? "○○○" : (liveGame.outs) ? "●○○" : "●●○"}</div>
+                    <div class="count">{(displayedGameIndex in suspectedCrashedGameIndexes) ? "(Crashed?)" : (liveGame.outs===0) ? "○○○" : (liveGame.outs===1) ? "●○○" : "●●○"}</div>
                 </div>
                 <div class="character"><img src={`/src/lib/images/Characters/${(liveGame.half_inning === 1) ? characters[liveGame[`away_roster_${liveGame.pitcher}_char`]] : characters[liveGame[`away_roster_${liveGame.batter}_char`]]}.png`} alt="current batter/pitcher"></div>
                 <div class="offDef"><img src={`/src/lib/images/${(liveGame.half_inning === 1) ? "Baseball" : "Baseball_bat"}.png`} alt="offence or defence"></div>
