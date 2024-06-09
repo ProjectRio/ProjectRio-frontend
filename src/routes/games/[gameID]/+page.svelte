ec3695b50fe683d963e12232d058c351cc7e3af1
Nicholas Lannutti <nlannutti97@gmail.com>
2024-06-08 20:58:44 -0400

    Fix images

--- src/routes/games/[gameID]/+page.svelte
+++ src/routes/games/[gameID]/+page.svelte
@@ -147,7 +147,7 @@
 {#if !loadingInd}
     <div class="headerBox">
         <div>
-            <img src={`/src/lib/images/Teams/${homeLogo}.png`} alt="Home team is the {homeLogo}">
+            <img src={`/images/Teams/${homeLogo}.png`} alt="Home team is the {homeLogo}">
             <div class="elo">Elo {homeElo}</div>
         </div>
         <div class="userAndScoreHome">
@@ -164,7 +164,7 @@
             <span class="score">{scoreAway}</span>
         </div>
         <div>
-            <img src={`/src/lib/images/Teams/${awayLogo}.png`} alt="Away team is the {awayLogo}">
+            <img src={`/images/Teams/${awayLogo}.png`} alt="Away team is the {awayLogo}">
             <div class="elo">Elo {awayElo}</div>
         </div>
     </div>
