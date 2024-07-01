e4b6374cc2f460af0ade8f9104b9271569769ce8
Nicholas Lannutti <nlannutti97@gmail.com>
2024-07-01 14:51:42 -0400

    Added d3 framework

--- src/routes/visualizations/+page.svelte
+++ src/routes/visualizations/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
     import { generatePitchCoordinates } from "$lib/helpers/MssbFunctions/PitchSimulation/generatePitchCoordinates";
+    import * as d3 from 'd3'
+	import { X } from "lucide-svelte";
+	import { onMount } from "svelte";
 
-    let pitchCoords:any = generatePitchCoordinates() 
+    let pitchData:any = generatePitchCoordinates() 
+    let plotData:any[] = [];
+
+    for (let i = 0; i < pitchData.calculatedPoints; i++) {
+        plotData.push({x: pitchData.calculatedPoints[i].X, z: pitchData.calculatedPoints[i].Z})
+    }
+
+    //const line = d3.line((d) => x(d.Date), (d) => yield(d.Close))
 </script>
 
+
+<!--svg width="500" height = "400"></svg-->
 <div>
     Testing
-    {JSON.stringify(pitchCoords)}
-</div>
+    {JSON.stringify(pitchData)}
+</div><br>
