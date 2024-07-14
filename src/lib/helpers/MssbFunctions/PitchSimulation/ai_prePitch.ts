555a082d836c37aa53d2c035754a12ba381ab51f
Nicholas Lannutti <nlannutti97@gmail.com>
2024-07-13 22:53:15 -0400

    Simplified.

--- src/lib/helpers/MssbFunctions/PitchSimulation/ai_prePitch.ts
+++ src/lib/helpers/MssbFunctions/PitchSimulation/ai_prePitch.ts
@@ -1,17 +1,17 @@
 import { WeightedRandomIndex } from "../mssbHelpers";
-import { pitcherAISelectPitch } from "./ai_selectPitch";
-import { pitcherAISetCurve } from "./ai_setCurve";
+import { ai_selectPitch } from "./ai_selectPitch";
+import { ai_setCurve } from "./ai_setCurve";
 import * as c from "./pitchingConstants";
 
 // @ts-ignore
-export function pitcherAI_prePitchSetConstants(dt) { //80660640
+export function ai_prePitch(dt) { //80660640
   let randomNum:any;
   let moundMovementNeeded:number;
   
                     /* On first frame, decide where the pitcher will stand on mound. */
-  if (dt.inMemPitcher.CountsUpWhileWaitingForPitch == 1) {
+  //if (dt.inMemPitcher.CountsUpWhileWaitingForPitch == 1) {
     if (dt.pitcherAIPitchDownTheMiddleInd == 0) {
-      randomNum = WeightedRandomIndex(c.aIMoundLocationProbabilities[dt.inMemPitcher.charClass],6, dt.StaticRandomInt1, dt.StaticRandomInt2, dt.TotalframesAtPlay);
+      [randomNum, dt] = WeightedRandomIndex(c.aIMoundLocationProbabilities[dt.inMemPitcher.charClass], 6, dt);
       if (randomNum != 5) {
         dt.aIMoundLocationIndex = randomNum;
         dt.aIMoundLocationX =
@@ -21,10 +21,11 @@ export function pitcherAI_prePitchSetConstants(dt) { //80660640
     else {
       dt.aIMoundLocationX = 0;
     }
-  }
-  if (29 < dt.inMemPitcher.CountsUpWhileWaitingForPitch) {
+  //}
+  dt.inMemPitcher.pitcherX = dt.aIMoundLocationX; // only need this instead of the following block.
+  /*if (29 < dt.inMemPitcher.CountsUpWhileWaitingForPitch) {
                     /* If pitcher isn't at the desired location, move them 0.08m/frame */
-    if (dt.aIMoundLocationX != dt.inMemPitcher.pitcherX) {
+    /*if (dt.aIMoundLocationX != dt.inMemPitcher.pitcherX) {
       moundMovementNeeded = dt.aIMoundLocationX - dt.inMemPitcher.pitcherX;
       if (moundMovementNeeded <= 0) {
         if (moundMovementNeeded < -0.08) {
@@ -41,11 +42,11 @@ export function pitcherAI_prePitchSetConstants(dt) { //80660640
         dt.inMemPitcher.pitcherX = dt.aIMoundLocationX;
       }
     }
-  }
+  }*/
   if ((true /*AIFrameToBeginPitch <= dt.inMemPitcher.CountsUpWhileWaitingForPitch*/) &&
      (true /*inMemBatter.someAnimationInd_beginningOfABAnimation? == 0*/)) {
-    pitcherAISelectPitch(dt);
-    pitcherAISetCurve(dt);
+    ai_selectPitch(dt);
+    ai_setCurve(dt);
     //pitcherAITransitionFromPrePitchToWindup(2);
     //pitchCodeUsedForReplays = realPitch;
   }
