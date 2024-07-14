7f0bc8f0e2bd94f5f973f1968a05cbef7960b2b6
Nicholas Lannutti <nlannutti97@gmail.com>
2024-07-13 22:53:53 -0400

    Fixed RNG outputs

--- src/lib/helpers/MssbFunctions/PitchSimulation/ai_setCurve.ts
+++ src/lib/helpers/MssbFunctions/PitchSimulation/ai_setCurve.ts
@@ -2,7 +2,7 @@ import { randBetween, RandomInt_Game } from "../mssbHelpers";
 import * as c from "./pitchingConstants";
 
 // @ts-ignore
-export function pitcherAISetCurve(dt) {
+export function ai_setCurve(dt) {
   let bVar1;
   let rng;
   let finalLocIndex;
@@ -19,18 +19,18 @@ export function pitcherAISetCurve(dt) {
     dt.aiPitchCurveEndingX = 0;
   }
   else if (dt.aiPitchCurveType == 2) {
-    moundLoc = randBetween(0xfffffff6,10, dt.StaticRandomInt1, dt.StaticRandomInt2, dt.TotalframesAtPlay);
+    [moundLoc, dt] = randBetween(-10,10, dt);
     dt.aiPitchCurveEndingX = 0.01 * moundLoc;
   }
   else if (dt.aiPitchCurveType == 3) {
     loopIndex = 0;
-    rng = RandomInt_Game(100, dt.StaticRandomInt1, dt.StaticRandomInt2, dt.TotalframesAtPlay);
+    [rng, dt] = RandomInt_Game(100, dt);
                     /* There are 7 positions for the ending loc of the pitch. I believe if the RNG
                        indicates a strong curve, then this algo randomly sets the ending position at
                        least 2 away from the mound position. */
     if (c.aiPitchWeakCurveProbabilities[dt.aIDifficultyInverse0Weak] <= rng) {
       do { // edited original function to get rid of jumps.
-        rng = RandomInt_Game(4, dt.StaticRandomInt1, dt.StaticRandomInt2, dt.TotalframesAtPlay);
+        [rng, dt] = RandomInt_Game(4, dt);
         moundLoc = dt.aIMoundLocationIndex;
         if (moundLoc == 0) {finalLocIndex = rng + 3}
         else {finalLocIndex = ((rng < moundLoc) ? rng : rng + 3)}
@@ -92,7 +92,7 @@ checkIfDiffLocThanLastTime:
                        the left or right.
                        It will try 3 times total before it would repeat the same location as the
                        previous pitch. */
-      rng = RandomInt_Game(3, dt.StaticRandomInt1, dt.StaticRandomInt2, dt.TotalframesAtPlay);
+      [rng, dt] = RandomInt_Game(3, dt);
       finalLocIndex = dt.aIMoundLocationIndex + rng;
       if ((dt.aIPitchDesiredEndingLocIndex != finalLocIndex) || (1 < loopIndex)) break;
       loopIndex = loopIndex + 1;
@@ -103,7 +103,7 @@ checkIfDiffLocThanLastTime:
   }
   dt.pitchAIDelayCurveStart = 0;
   bVar1 = c.pitchAIDelayPitchStartProbabilities[dt.inMemPitcher.charClass][dt.aIDifficultyInverse0Weak];
-  rng = RandomInt_Game(100, dt.StaticRandomInt1, dt.StaticRandomInt2, dt.TotalframesAtPlay);
+  [rng, dt] = RandomInt_Game(100, dt);
   if (bVar1 < rng) {
     dt.pitchAIDelayCurveStart = 1;
   }
